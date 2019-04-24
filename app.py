d6b8e4d1b705af615f2cdeb3dd8a3649cbf2c50b
George Higbie <ghigbie@monigle.com>
2019-04-24 17:50:29 -0600

    Add methods

--- app.py
+++ app.py
@@ -2,8 +2,20 @@ from flask import Flask
 from falsk_restful import Resource, Api
 
 app = Flask (__name__)
-
 api = Api(app)
 
+puppies = []
+
+class PuppyNames(Resource):
+
+    def get(self):
+        pass
+    
+    def post():
+        pass
+    
+    def delete():
+        pass
+        
 if __name__ == '__main__':
     app.run(Debug=True)
