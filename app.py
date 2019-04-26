321fcc36876c87e0573c7a2fd1b8cc131ce63c60
ghigbie <gehigbie@gmail.com>
2019-04-25 21:38:56 -0600

    Edit config for database

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+import os
 from flask import Flask
 from flask_restful import Resource, Api
 from secure_check import authenticate, identity
 from flask_jwt import JWT, jwt_required
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask (__name__)
+
 app.config['SECRET_KEY'] = 'mysecretkey'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
