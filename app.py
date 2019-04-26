d19e72f4970ea4632e7797610d01da9462830047
ghigbie <gehigbie@gmail.com>
2019-04-25 20:26:48 -0600

    Edit function mapping

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ class AllNames(Resource):
         return {'puppies': puppies}
 
 api.add_resource(PuppyNames, '/puppy/<string:name>')
-api.add_resource(AllNames, '/')
 api.add_resource(AllNames, '/puppies')
-api.add_resource(AllNames, '/allnames')
+
+
 if __name__ == '__main__':
     app.run(Debug=True)
